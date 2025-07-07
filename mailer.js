77022036a02dec4848348b080d010946d8e923cc
Aneetta2211 <aneettajose1@gmail.com>
2025-07-07 17:43:01 +0530

    updated mailer

--- mailer.js
+++ mailer.js
@@ -1,8 +1,8 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config({ path: __dirname + "/.env" }); 
 
-const email = (process.env.EMAIL || "").trim();
-const password = (process.env.EMAIL_PASSWORD || "").trim();
+const email = (process.env.EMAIL || "aneettajose1@gmail.com").trim();
+const password = (process.env.EMAIL_PASSWORD || "bxgk usjd flar gtfh").trim();
 
 if (!email || !password) {
     console.warn("EMAIL or EMAIL_PASSWORD is missing! Mailer will not be configured.");
