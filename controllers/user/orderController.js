dbf60af35f201101fe38ab315f716fe93c94582c
Aneetta2211 <aneettajose1@gmail.com>
2025-07-15 13:08:51 +0530

    Fix: fixed order detail

--- controllers/user/orderController.js
+++ controllers/user/orderController.js
@@ -594,7 +594,7 @@ const downloadInvoice = async (req, res) => {
 };
 const getOrderDetails = async (req, res) => {
     try {
-        const orderId = req.body.orderId;
+        const orderId = req.params.orderID;
         const userId = req.user?._id || req.session.user?._id;
 
         console.log("Fetching order details for:", orderId, "by user:", userId);
